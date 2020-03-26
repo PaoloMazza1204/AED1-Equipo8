0c9a81569f72b8858a20316e74734358d0c0c083
Paolo Mazza <paolo.a.mazza@gmail.com>
2020-03-26 14:41:58 -0300

    Fix

--- UT1/TA1/PruebaAtributos.java
+++ UT1/TA1/PruebaAtributos.java
@@ -76,7 +76,7 @@ class PrePostDemo {
 1. aValue = 8933.234; (b)
 2. aValue++; (a)
 3. System.out.println("Hello World!"); (d)
-4. Bicycle myBike = new Bicycle(); (c) y (b)
+4. Bicycle myBike = new Bicycle(); (c)
 
 
 a) Incremento
