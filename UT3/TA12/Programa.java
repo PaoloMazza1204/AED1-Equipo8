549fb65a01e63480de6865b346cad5c6c2bd0bb3
Paolo Mazza <paolo.a.mazza@gmail.com>
2020-04-22 16:28:38 -0300

    Pruebas descriptivas

--- UT3/TA12/Programa.java
+++ UT3/TA12/Programa.java
@@ -17,37 +17,41 @@ public class Programa {
         miPila.push(nodo3);
         miPila.push(nodo4);
 
-        System.out.println(miPila.esVacia());
+        System.out.println("Pila de 4 elementos es vacía? " + miPila.esVacia());
 
-        System.out.println(miPila.topeDato());
-        System.out.println(miPila.topeEtiqueta());
+        System.out.println("tope debería ser 4, tope: " + miPila.topeDato());
+        System.out.println("Etiqueta del tope debería ser 4, tope: " +
+                            miPila.topeEtiqueta());
 
         miPila.anula();
-        System.out.println(miPila.esVacia());
+        System.out.println("Vaciamos la pila.");
+        System.out.println("Pila vacía luego de vaciarla? " + miPila.esVacia());
 
 
         // Cola
-        System.out.println("Pruebas de cola");
-        Cola<Integer> miCola = new Cola<>();
+        System.out.println('\n' + "Pruebas de cola");
+        Cola<Integer> cola = new Cola<>();
 
         Nodo<Integer> nodo1_ = new Nodo<>(1,1);
         Nodo<Integer> nodo2_ = new Nodo<>(2,2);
         Nodo<Integer> nodo3_ = new Nodo<>(3,3);
         Nodo<Integer> nodo4_ = new Nodo<>(4,4);
 
-        System.out.println(miCola.esVacia()); // true
+        System.out.println("Cola vacía es vacía? " + cola.esVacia());
 
-        miCola.agregar(nodo1_);
-        miCola.agregar(nodo2_);
-        miCola.agregar(nodo3_);
-        miCola.agregar(nodo4_);
+        cola.agregar(nodo1_);
+        cola.agregar(nodo2_);
+        cola.agregar(nodo3_);
+        cola.agregar(nodo4_);
 
-        System.out.println(miCola.esVacia()); // false
-        System.out.println(miCola.frenteEtiqueta()); // 1
-        miCola.quitar();
-        System.out.println(miCola.frenteEtiqueta()); // 2
+        System.out.println("Cola de 4 elementos es vacía? " + cola.esVacia());
+        System.out.println("Primer elemento debería ser 1 y es " + cola.frenteEtiqueta());
+        cola.quitar();
+        System.out.println("Quitamos un elemento");
+        System.out.println("Primer elemento debería ser 2 y es " + cola.frenteEtiqueta());
 
-        miCola.anular();
-        System.out.println(miCola.esVacia()); // true
+        cola.anular();
+        System.out.println("Anulamos la cola");
+        System.out.println("Cola vacía es vacía? " + cola.esVacia());
     }
 }
